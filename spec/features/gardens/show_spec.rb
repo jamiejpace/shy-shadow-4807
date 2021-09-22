e95322344517d40f8ab880c1cddc0ba577507b52
Jamie Pace <jamiejpace@gmail.com>
2021-09-22 11:01:13 -0600

    Complete third user story with tests

--- spec/features/gardens/show_spec.rb
+++ spec/features/gardens/show_spec.rb
@@ -8,11 +8,22 @@ RSpec.describe 'garden show page' do
     @plant1 = Plant.create!(name: "Golden Tomato", description: "Medium sun", days_to_harvest: 50)
     @plant2 = Plant.create!(name: "Special Lettuce", description: "Mostly shade, lots of water", days_to_harvest: 60)
     @plant3 = Plant.create!(name: "String Beans", description: "Medium sun, rich soil", days_to_harvest: 70)
+    @plant4 = Plant.create!(name: "Blue Watermelon", description: "Full sun, dry soil", days_to_harvest: 150)
     @plotplant1 = PlotPlant.create!(plant_id: @plant1.id, plot_id: @plot1.id)
     @plotplant1 = PlotPlant.create!(plant_id: @plant2.id, plot_id: @plot1.id)
     @plotplant1 = PlotPlant.create!(plant_id: @plant3.id, plot_id: @plot1.id)
     @plotplant1 = PlotPlant.create!(plant_id: @plant3.id, plot_id: @plot2.id)
-    visit plots_path
+    @plotplant1 = PlotPlant.create!(plant_id: @plant4.id, plot_id: @plot2.id)
+    visit garden_path(@garden.id)
+  end
+
+  it 'lists unique plants for the garden that take less than 100 days to harvest' do
+    save_and_open_page
+    
+    expect(page).to have_content(@plant1.name, count: 1)
+    expect(page).to have_content(@plant2.name)
+    expect(page).to have_content(@plant3.name)
+    expect(page).to_not have_content(@plant4.name)
   end
 
 end
--- spec/models/garden_spec.rb
+++ spec/models/garden_spec.rb
@@ -4,4 +4,28 @@ RSpec.describe Garden do
   describe 'relationships' do
     it { should have_many(:plots) }
   end
+
+  describe 'instance methods' do
+    describe '.unique_plants_short_harvest' do
+      before :each do
+        @garden = Garden.create!(name: "Jamie's Garden", organic: true)
+        @plot1 = @garden.plots.create!(number: 1, size: "Large", direction: "West")
+        @plot2 = @garden.plots.create!(number: 2, size: "Small", direction: "East")
+        @plant1 = Plant.create!(name: "Golden Tomato", description: "Medium sun", days_to_harvest: 50)
+        @plant2 = Plant.create!(name: "Special Lettuce", description: "Mostly shade, lots of water", days_to_harvest: 60)
+        @plant3 = Plant.create!(name: "String Beans", description: "Medium sun, rich soil", days_to_harvest: 70)
+        @plant4 = Plant.create!(name: "Blue Watermelon", description: "Full sun, dry soil", days_to_harvest: 150)
+        @plotplant1 = PlotPlant.create!(plant_id: @plant1.id, plot_id: @plot1.id)
+        @plotplant1 = PlotPlant.create!(plant_id: @plant2.id, plot_id: @plot1.id)
+        @plotplant1 = PlotPlant.create!(plant_id: @plant3.id, plot_id: @plot1.id)
+        @plotplant1 = PlotPlant.create!(plant_id: @plant3.id, plot_id: @plot2.id)
+        @plotplant1 = PlotPlant.create!(plant_id: @plant4.id, plot_id: @plot2.id)
+      end
+
+      it 'find all the unique plants for a garden that have a harvest time less than 100 days and return the plant name' do
+        expect(@garden.unique_plants_short_harvest.length).to eq(3)
+        expect(@garden.unique_plants_short_harvest.first).to eq(@plant1.name)
+      end
+    end
+  end
 end
